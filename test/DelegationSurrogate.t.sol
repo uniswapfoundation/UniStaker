845720269d49c9f9c3043fc43439baee38eae8c4
Ben DiFrancesco <ben@scopelift.co>
2023-12-07 06:37:48 -0500

    Replace template placeholder with surrogate contract files

---
+++ test/DelegationSurrogate.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.23;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {DelegationSurrogate} from "src/DelegationSurrogate.sol";
+
+contract DelegationSurrogateTest is Test {
+  function setUp() public { }
+}
+
