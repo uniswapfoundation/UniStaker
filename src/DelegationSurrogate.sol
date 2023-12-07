e0edbaf6f572dda5948ce21b360967132b6b9ef3
Ben DiFrancesco <ben@scopelift.co>
2023-12-07 07:36:07 -0500

    Implement and test the DelegationSurrogate contract

--- src/DelegationSurrogate.sol
+++ src/DelegationSurrogate.sol
@@ -1,5 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.23;
 
+import {IERC20Delegates} from "src/interfaces/IERC20Delegates.sol";
+
 contract DelegationSurrogate {
+  constructor(IERC20Delegates _token, address _delegatee) {
+    _token.delegate(_delegatee);
+    _token.approve(msg.sender, type(uint256).max);
+  }
 }
--- test/DelegationSurrogate.t.sol
+++ test/DelegationSurrogate.t.sol
@@ -3,8 +3,48 @@ pragma solidity 0.8.23;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {DelegationSurrogate} from "src/DelegationSurrogate.sol";
+import {MockERC20Votes} from "test/mocks/MockERC20Votes.sol";
 
 contract DelegationSurrogateTest is Test {
-  function setUp() public { }
+  MockERC20Votes govToken;
+
+  function setUp() public {
+    govToken = new MockERC20Votes();
+    vm.label(address(govToken), "Governance Token");
+  }
+
+  function __deploy(address _deployer, address _delegatee) public returns (DelegationSurrogate) {
+    vm.assume(_deployer != address(0));
+
+    vm.prank(_deployer);
+    DelegationSurrogate _surrogate = new DelegationSurrogate(govToken, _delegatee);
+    return _surrogate;
+  }
 }
 
+contract Constructor is DelegationSurrogateTest {
+  function test_DelegatesToDeployer(address _deployer, address _delegatee) public {
+    DelegationSurrogate _surrogate = __deploy(_deployer, _delegatee);
+    assertEq(_delegatee, govToken.delegates(address(_surrogate)));
+  }
+
+  function test_MaxApprovesDeployerToEnableWithdrawals(
+    address _deployer,
+    address _delegatee,
+    uint256 _amount,
+    address _receiver
+  ) public {
+    vm.assume(_receiver != address(0));
+
+    DelegationSurrogate _surrogate = __deploy(_deployer, _delegatee);
+    govToken.mint(address(_surrogate), _amount);
+
+    uint256 _allowance = govToken.allowance(address(_surrogate), _deployer);
+    assertEq(_allowance, type(uint256).max);
+
+    vm.prank(_deployer);
+    govToken.transferFrom(address(_surrogate), _receiver, _amount);
+
+    assertEq(govToken.balanceOf(_receiver), _amount);
+  }
+}
