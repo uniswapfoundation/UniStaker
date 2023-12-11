dc1490901a6b573ce85912ca1658454f9fb5588d
Ben DiFrancesco <ben@scopelift.co>
2023-12-11 12:31:27 -0500

    Set up the UniStaker contract & test harness

---
+++ test/fakes/ERC20Fake.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.23;
+
+import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
+
+/// @dev An ERC20 token that allows for public minting for use in tests.
+contract ERC20Fake is ERC20 {
+  constructor() ERC20("Fake Token", "FAKE") {}
+
+  /// @dev Public mint function useful for testing
+  function mint(address _account, uint256 _value) public {
+    _mint(_account, _value);
+  }
+}
