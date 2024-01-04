31e7aed7725d81ad65d20d2d0ac086d7d313145e
Ben DiFrancesco <ben@scopelift.co>
2024-01-04 13:57:16 -0500

    Implement and test a method to update the admin

--- src/V3FactoryOwner.sol
+++ src/V3FactoryOwner.sol
@@ -2,9 +2,23 @@
 pragma solidity 0.8.23;
 
 contract V3FactoryOwner {
-    address public admin;
+  address public admin;
 
-    constructor(address _admin) {
-        admin = _admin;
-    }
+  event AdminUpdated(address indexed oldAmin, address indexed newAdmin);
+
+  error V3FactoryOwner__Unauthorized();
+
+  constructor(address _admin) {
+    admin = _admin;
+  }
+
+  function setAdmin(address _newAdmin) external {
+    _revertIfNotAdmin();
+    emit AdminUpdated(admin, _newAdmin);
+    admin = _newAdmin;
+  }
+
+  function _revertIfNotAdmin() internal view {
+    if (msg.sender != admin) revert V3FactoryOwner__Unauthorized();
+  }
 }
--- test/V3FactoryOwner.t.sol
+++ test/V3FactoryOwner.t.sol
@@ -5,24 +5,50 @@ import {Test, console2} from "forge-std/Test.sol";
 import {V3FactoryOwner} from "src/V3FactoryOwner.sol";
 
 contract V3FactoryOwnerTest is Test {
-    V3FactoryOwner factoryOwner;
-    address admin = address(0xb055beef);
+  V3FactoryOwner factoryOwner;
+  address admin = makeAddr("Admin");
 
-    function setUp() public {
-        vm.label(admin, "Admin");
+  event AdminUpdated(address indexed oldAmin, address indexed newAdmin);
 
-        factoryOwner = new V3FactoryOwner(admin);
-        vm.label(address(factoryOwner), "Factory Owner");
-    }
+  function setUp() public {
+    factoryOwner = new V3FactoryOwner(admin);
+    vm.label(address(factoryOwner), "Factory Owner");
+  }
 }
 
 contract Constructor is V3FactoryOwnerTest {
-    function test_SetsTheAdmin() public {
-        assertEq(factoryOwner.admin(), admin);
-    }
+  function test_SetsTheAdmin() public {
+    assertEq(factoryOwner.admin(), admin);
+  }
 
-    function testFuzz_SetTheAdminToAnArbitraryAddress(address _admin) public {
-        V3FactoryOwner _factoryOwner = new V3FactoryOwner(_admin);
-        assertEq(_factoryOwner.admin(), _admin);
-    }
+  function testFuzz_SetTheAdminToAnArbitraryAddress(address _admin) public {
+    V3FactoryOwner _factoryOwner = new V3FactoryOwner(_admin);
+    assertEq(_factoryOwner.admin(), _admin);
+  }
+}
+
+contract SetAdmin is V3FactoryOwnerTest {
+  function testFuzz_UpdatesTheAdminWhenCalledByTheCurrentAdmin(address _newAdmin) public {
+    vm.prank(admin);
+    factoryOwner.setAdmin(_newAdmin);
+
+    assertEq(factoryOwner.admin(), _newAdmin);
+  }
+
+  function testFuzz_EmitsAnEventWhenUpdatingTheAdmin(address _newAdmin) public {
+    vm.expectEmit(true, true, true, true);
+    vm.prank(admin);
+    emit AdminUpdated(admin, _newAdmin);
+    factoryOwner.setAdmin(_newAdmin);
+  }
+
+  function testFuzz_RevertIf_TheCallerIsNotTheCurrentAdmin(address _notAdmin, address _newAdmin)
+    public
+  {
+    vm.assume(_notAdmin != admin);
+
+    vm.expectRevert(V3FactoryOwner.V3FactoryOwner__Unauthorized.selector);
+    vm.prank(_notAdmin);
+    factoryOwner.setAdmin(_newAdmin);
+  }
 }
