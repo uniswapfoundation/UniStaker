6114e9b9f3810e9f7e3154960eaab1e4bcfb01a7
Ben DiFrancesco <ben@scopelift.co>
2024-01-06 17:06:25 -0500

    Allow the factory owner to transfer ownership of the factory

--- src/interfaces/IUniswapV3FactoryOwnerActions.sol
+++ src/interfaces/IUniswapV3FactoryOwnerActions.sol
@@ -2,18 +2,20 @@
 pragma solidity ^0.8.23;
 
 /// @title The interface for the Uniswap V3 Factory
-/// @notice The Uniswap V3 Factory facilitates creation of Uniswap V3 pools and control over the protocol fees
+/// @notice The Uniswap V3 Factory facilitates creation of Uniswap V3 pools and control over the
+/// protocol fees
 /// @dev Stripped down and renamed from:
 /// https://github.com/Uniswap/v3-core/blob/d8b1c635c275d2a9450bd6a78f3fa2484fef73eb/contracts/interfaces/IUniswapV3Factory.sol
 interface IUniswapV3FactoryOwnerActions {
-/// @notice Updates the owner of the factory
-    /// @dev Must be called by the current owner
-    /// @param _owner The new owner of the factory
-    function setOwner(address _owner) external;
+  /// @notice Updates the owner of the factory
+  /// @dev Must be called by the current owner
+  /// @param _owner The new owner of the factory
+  function setOwner(address _owner) external;
 
-    /// @notice Enables a fee amount with the given tickSpacing
-    /// @dev Fee amounts may never be removed once enabled
-    /// @param fee The fee amount to enable, denominated in hundredths of a bip (i.e. 1e-6)
-    /// @param tickSpacing The spacing between ticks to be enforced for all pools created with the given fee amount
-    function enableFeeAmount(uint24 fee, int24 tickSpacing) external;
+  /// @notice Enables a fee amount with the given tickSpacing
+  /// @dev Fee amounts may never be removed once enabled
+  /// @param fee The fee amount to enable, denominated in hundredths of a bip (i.e. 1e-6)
+  /// @param tickSpacing The spacing between ticks to be enforced for all pools created with the
+  /// given fee amount
+  function enableFeeAmount(uint24 fee, int24 tickSpacing) external;
 }
