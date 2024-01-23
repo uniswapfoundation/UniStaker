6dd04c5acdea0281c29a50f8cd6fbece83781021
Ben DiFrancesco <ben@scopelift.co>
2024-01-23 14:51:07 -0500

    Explicitly implement reward receiver interface on UniStaker contract
    The UniStake contract is updated to explicitly conform to the INotifiableRewardReceiver
    interface. This does not add any functionality, but ensures at compile time that
    the contract's interface will remain in sync with the one expected by the V3FactoryOwner
    contract. We also add natspec documentation for this interface.

--- src/interfaces/INotifiableRewardReceiver.sol
+++ src/interfaces/INotifiableRewardReceiver.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.23;
 
+/// @title INotifiableRewardReceiver
+/// @author ScopeLift
+/// @notice The communication interface between the V3FactoryOwner contract and the UniStaker
+/// contract. In particular, the V3FactoryOwner only needs to know the latter implements the
+/// specified method in order to forward payouts to the UniStaker contract. The UniStaker contract
+/// receives the rewards and abstracts the distribution mechanics
 interface INotifiableRewardReceiver {
+  /// @notice Method called to notify a reward receiver it has received a reward.
+  /// @param _amount The amount of reward.
   function notifyRewardsAmount(uint256 _amount) external;
 }
