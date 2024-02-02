85d8235ef6ac19e8763ed2bd0a0b23ed681a409b
keating <keating.dev@protonmail.com>
2024-02-02 16:16:29 -0500

    WIP: Deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,8 +3,29 @@
 
 pragma solidity 0.8.23;
 
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
 import {Script} from "forge-std/Script.sol";
 
-contract Deploy is Script {
-  function run() public {}
+import {DeployInput} from "script/DeployInput.sol";
+import {UniStaker} from "src/UniStaker.sol";
+import {V3FactoryOwner} from "src/V3FactoryOwner.sol";
+import {IERC20Delegates} from "src/interfaces/IERC20Delegates.sol";
+import {INotifiableRewardReceiver} from "src/interfaces/INotifiableRewardReceiver";
+import {IUniswapV3FactoryOwnerActions} from "src/interfaces/IUniswapV3FactoryOwnerActions.sol";
+
+contract Deploy is Script, DeployInput {
+  function run() public {
+		  // Deploy the staking contract
+		  UniStaker uniStaker = new UniStaker(IERC20(PAYOUT_TOKEN_ADDRESS), IERC20Delegates(STAKE_TOKEN_ADDRESS), msg.sender);
+
+		  // Deploy a new owner for the V3 factory
+		  // Some outside part of the proposal will switch this to the v3 factory
+		  address v3FactoryOwner = new V3FactoryOwner(UNISWAP_GOVERNOR_TIMELOCK, IUniswapV3FactoryOwnerActions(), IERC20(PAYOUT_TOKEN_ADDRESS), _payoutAmount, INotifiableRewardReceiver(uniStaker));
+
+		  // Set the v3FactoryOwner to be the rewards notifier
+		  uniStaker.setRewardsNotifier(v3FactoryOwner, true);
+
+		  // Change the Staking contract to be the governor timelock
+		  uniStaker.setAdmin(UNISWAP_GOVERNOR_TIMELOCK);
+  }
 }
---
+++ script/DeployInput.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// slither-disable-start reentrancy-benign
+
+pragma solidity 0.8.23;
+
+contract DeployInput {
+  address constant UNISWAP_GOVERNOR_TIMELOCK = 0x1a9C8182C09F50C8318d769245beA52c32BE35BC; // TODO double check: currently the Uniswap timelock 
+  address constant UNISWAP_V3_OWNER_FACTORY_ADDRESS = 0x1F98431c8aD98523631AE4a59f267346ea31F984; // TODO double check
+  address constant PAYOUT_TOKEN_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // TODO double check: currently USDC
+  address constant STAKE_TOKEN_ADDRESS = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984; // TODO double check: currently the UNI token
+  uint256 constant PAYOUT_AMOUNT = 50_0000e6; // TODO double check: currently 50_000 USDC
+}
