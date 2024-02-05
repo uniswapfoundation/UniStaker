f7411d9b838d5d00d08f1f747d8176183e77f85e
keating <keating.dev@protonmail.com>
2024-02-05 08:35:52 -0500

    Update comment

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -20,9 +20,9 @@ contract Deploy is Script, DeployInput {
     UniStaker uniStaker =
       new UniStaker(IERC20(PAYOUT_TOKEN_ADDRESS), IERC20Delegates(STAKE_TOKEN_ADDRESS), msg.sender);
 
-    // Deploy a new owner for the V3 factory
-    // Some outside part of the proposal will switch the owner of the
-	// v3 contract to this contract.
+    // Deploy a new owner for the V3 factory owner actions contract.
+    // A separate par of the proposal will switch the owner of the
+	// v3 factory owner actions contract contract to the v3 factory owner.
     V3FactoryOwner v3FactoryOwner = new V3FactoryOwner(
       UNISWAP_GOVERNOR_TIMELOCK,
       IUniswapV3FactoryOwnerActions(UNISWAP_V3_OWNER_FACTORY_ADDRESS),
