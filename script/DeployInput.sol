5236f93f5cb62ec96a1c45af41b47f537d6c467d
keating <keating.dev@protonmail.com>
2024-02-06 09:50:39 -0500

    Changes based on feedback

--- script/DeployInput.sol
+++ script/DeployInput.sol
@@ -4,14 +4,11 @@
 pragma solidity 0.8.23;
 
 contract DeployInput {
-  // TODO double check: currently the Uniswap timelock
   address constant UNISWAP_GOVERNOR_TIMELOCK = 0x1a9C8182C09F50C8318d769245beA52c32BE35BC;
-  // TODO double check
   address constant UNISWAP_V3_FACTORY_ADDRESS = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
-  // TODO double check: currently USDC
-  address constant PAYOUT_TOKEN_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-  // TODO double check: currently the UNI token
+  // TODO not finalized: currently WETH
+  address constant PAYOUT_TOKEN_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
   address constant STAKE_TOKEN_ADDRESS = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;
-  // TODO double check: currently 50_000 USDC
-  uint256 constant PAYOUT_AMOUNT = 50_000e6;
+  // TODO not determined yet
+  uint256 constant PAYOUT_AMOUNT = 10e18;
 }
