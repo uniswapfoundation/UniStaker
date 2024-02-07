5e89d24116f7e1ee2c89e8117b0d6a5188ace7c8
Ben DiFrancesco <ben@scopelift.co>
2024-02-07 10:47:12 -0500

    Implement, test, and document permit-based stake & stakeMore methods

--- test/mocks/MockERC20Votes.sol
+++ test/mocks/MockERC20Votes.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.23;
 
-import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
 import {IERC20Delegates} from "src/interfaces/IERC20Delegates.sol";
+import {ERC20, ERC20Permit} from "openzeppelin/token/ERC20/extensions/ERC20Permit.sol";
 
-/// @dev An ERC20 token that allows for public minting and mocks the delegation methods used in
-/// ERC20Votes governance tokens. It does not included check pointing functionality. This contract
-/// is intended only for use as a stand in for contracts that interface with ERC20Votes tokens.
-contract ERC20VotesMock is IERC20Delegates, ERC20 {
+/// @dev An ERC20Permit token that allows for public minting and mocks the delegation methods used
+/// in ERC20Votes governance tokens. It does not included check pointing functionality. This
+/// contract is intended only for use as a stand in for contracts that interface with ERC20Votes
+// tokens.
+contract ERC20VotesMock is IERC20Delegates, ERC20Permit {
   /// @dev Track delegations for mocked delegation methods
   mapping(address account => address delegate) private delegations;
 
-  constructor() ERC20("Governance Token", "GOV") {}
+  constructor() ERC20("Governance Token", "GOV") ERC20Permit("Governance Token") {}
 
   /// @dev Public mint function useful for testing
   function mint(address _account, uint256 _value) public {
@@ -87,5 +88,17 @@ contract ERC20VotesMock is IERC20Delegates, ERC20 {
     returns (bool)
   {
     return ERC20.transferFrom(src, dst, rawAmount);
+  }
+
+  function permit(
+    address owner,
+    address spender,
+    uint256 rawAmount,
+    uint256 deadline,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) public override(IERC20Delegates, ERC20Permit) {
+    return ERC20Permit.permit(owner, spender, rawAmount, deadline, v, r, s);
   }
 }
