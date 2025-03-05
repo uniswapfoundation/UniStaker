ab1f12e5d24e7edc764bd2b42a126634d81f6494
saucepoint <saucepoint@protonmail.com>
2025-03-04 20:54:06 -0700

    adjust natspec

--- src/interfaces/INotifiableRewardReceiver.sol
+++ src/interfaces/INotifiableRewardReceiver.sol
@@ -8,7 +8,21 @@ pragma solidity ^0.8.23;
 /// specified method in order to forward payouts to the UniStaker contract. The UniStaker contract
 /// receives the rewards and abstracts the distribution mechanics
 interface INotifiableRewardReceiver {
-  /// @notice Method called to notify a reward receiver it has received a reward.
-  /// @param _amount The amount of reward.
+  /// @notice Called by an authorized rewards notifier to alert the staking contract that a new
+  /// reward has been transferred to it. It is assumed that the reward has already been transferred
+  /// to this staking contract before the rewards notifier calls this method.
+  /// @param _amount Quantity of reward tokens the staking contract is being notified of.
+  /// @dev It is critical that only well behaved contracts are approved by the admin to call this
+  /// method, for two reasons.
+  ///
+  /// 1. A misbehaving contract could grief stakers by frequently notifying this contract of tiny
+  /// rewards, thereby continuously stretching out the time duration over which real rewards are
+  /// distributed. It is required that reward notifiers supply reasonable rewards at reasonable
+  /// intervals.
+  ///
+  /// 2. A misbehaving contract could falsely notify this contract of rewards that were not actually
+  /// distributed, creating a shortfall for those claiming their rewards after others. It is
+  /// required that a notifier contract always transfers the `_amount` to this contract before
+  /// calling this method.
   function notifyRewardAmount(uint256 _amount) external;
 }
